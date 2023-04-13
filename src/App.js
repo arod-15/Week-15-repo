5b699d74724f2d49036f4307d0cd6870e0a44db6
alex reeder <alexreeder15@hotmail.com>
2023-04-13 13:11:59 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HousesList } from ',/Components/HousesList';
+
+class App extents Component {
+  render() {
+    return (
+      <div>
+        <HousesList />
+      </div>
+    )
+  }
+}
+
+export default App;
